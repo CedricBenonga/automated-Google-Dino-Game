301685882822df9a41a3c58c287bae6fee729ce1
Cedric Benonga <cedricbenonga13@gmail.com>
2023-07-20 22:51:18 +0200

    updated

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import time
 import pyautogui
 
+
+obstacle = (83, 83, 83)
 time.sleep(3)
 pyautogui.press('Up')
 
 while True:
-    if pyautogui.pixelMatchesColor(350, 650, (83, 83, 83)) or pyautogui.pixelMatchesColor(300, 640, (83, 83, 83)):
+    if pyautogui.pixelMatchesColor(350, 650, obstacle) or pyautogui.pixelMatchesColor(300, 640, obstacle):
         pyautogui.press('Up')
 
-    if pyautogui.pixelMatchesColor(350, 600, (83, 83, 83)) or pyautogui.pixelMatchesColor(300, 600, (83, 83, 83)):
+    if pyautogui.pixelMatchesColor(350, 600, obstacle) or pyautogui.pixelMatchesColor(300, 600, obstacle):
         pyautogui.press('Up')
 
-    if pyautogui.pixelMatchesColor(350, 530, (83, 83, 83)) or pyautogui.pixelMatchesColor(330, 530, (83, 83, 83)):
+    if pyautogui.pixelMatchesColor(350, 530, obstacle) or pyautogui.pixelMatchesColor(330, 530, obstacle):
         pyautogui.press('Down')
