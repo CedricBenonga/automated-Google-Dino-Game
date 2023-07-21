52937812d5c727d4b574d5a4d70f7267a7408d0c
Cedric Benonga <cedricbenonga13@gmail.com>
2023-07-21 06:37:50 +0200

    updated

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import pyautogui
 
 
 obstacle = (83, 83, 83)
-white_obstacle = (255, 255, 255)
+space_under_obs = (255, 255, 255)
 
 time.sleep(3)
 pyautogui.press('Up')
 
+w = 600
+x = 350
+y = 650
+z = 530
+
 while True:
-    w = 600
-    x = 350
-    y = 650
-    z = 530
     if pyautogui.pixelMatchesColor(x, y, obstacle):
         pyautogui.press('Up')
-    if pyautogui.pixelMatchesColor(x, w, obstacle) and pyautogui.pixelMatchesColor(x, y, white_obstacle):
+    if pyautogui.pixelMatchesColor(x, w, obstacle) and pyautogui.pixelMatchesColor(x, y, space_under_obs):
         pyautogui.press('Down')
     if pyautogui.pixelMatchesColor(x, z, obstacle):
         pyautogui.press('Down')
